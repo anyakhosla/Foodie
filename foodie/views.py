f3a5ba1332870a76c861152f53c671a9340ba2c1
aditikoratp <aditikoratp@gmail.com>
2024-09-15 16:12:29 -0400

    commit views.py

--- foodie/views.py
+++ foodie/views.py
@@ -8,3 +8,10 @@ def mapView(request, id):
     # question = get_object_or_404(Question, pk=id)
     # return render(request, "foodie/mapView.html", {"question": question})
     return render(request, "foodie/mapView.html")
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+@login_required
+def profile_view(request):
+    return render(request, 'profile.html', {'user': request.user})
