bf33c81cc733f5de5f2e0db9da0405771290c7c0
atharvakhandelwal <akhandelwal73@gatech.edu>
2024-09-15 18:49:53 -0400

    added css file styling as well as static to settings.py

--- foodie/urls.py
+++ foodie/urls.py
@@ -6,11 +6,11 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('', views.home, name='home'),  # Add your home view if needed
+    #path('', views.home, name='home'),  # Add your home view if needed
     # ... other URL patterns ...
     path("map", views.mapView, name = "mapView"),
-    path('profile/', views.profile_view, name='profile'),
-    path('restaurant-data/', views.restaurant_data, name='restaurant_data'),
+    #path('profile/', views.profile_view, name='profile'),
+    #path('restaurant-data/', views.restaurant_data, name='restaurant_data'),
 ]
 
 
