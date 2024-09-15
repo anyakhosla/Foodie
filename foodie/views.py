3a2e5ee00cb2d5c223334c1b4023ab05f4cd04c2
Anonymoustank <pkadekodi@gmail.com>
2024-09-15 16:36:04 -0400

    fix merge issue

--- foodie/views.py
+++ foodie/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -9,3 +10,7 @@ def mapView(request):
     # return render(request, "foodie/mapView.html", {"question": question})
 
     return render(request, "foodie/mapView.html", {})
+
+@login_required
+def profile_view(request):
+    return render(request, 'profile.html', {'user': request.user})
