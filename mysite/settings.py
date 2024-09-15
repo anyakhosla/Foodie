ecb2d2a25a6a3bf07965d209329cc6f2ee98a176
Anonymoustank <pkadekodi@gmail.com>
2024-09-15 18:54:59 -0400

    change map name + remove cache dirs

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,7 +124,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "mysite/static",
+    # BASE_DIR / "static",
 ]
 
 # Default primary key field type
