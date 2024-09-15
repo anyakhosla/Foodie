41cba5440410572d5b765292de2515f59849c66f
Anonymoustank <pkadekodi@gmail.com>
2024-09-15 15:59:37 -0400

    created mapview

--- foodie/views.py
+++ foodie/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+
+# from .models import Question, Choice
+
+def mapView(request, id):
+    # question = get_object_or_404(Question, pk=id)
+    # return render(request, "foodie/mapView.html", {"question": question})
+    return render(request, "foodie/mapView.html")
