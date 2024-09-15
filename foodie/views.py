e60d13e0117791b66a34378cdd50231d7d16bd81
aarushkachhawa <aarushkachhawa@gmail.com>
2024-09-15 17:33:31 -0400

    Finished up the views.py file and made minor edits to models.py and settings.py to be able to test our models in a python shell. Migrated models to database as well.

--- foodie/views.py
+++ foodie/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Restaurant
 
-# Create your views here.
-# starting views
+def restaurant_detail(request, restaurant_id):
+    # Fetch the restaurant by ID
+    restaurant = get_object_or_404(Restaurant, pk=restaurant_id)
+
+    # Fetching the reviews from the JSONField (assuming the reviews are stored in a list)
+    reviews = restaurant.reviews if restaurant.reviews else []
+
+    context = {
+        'restaurant': restaurant,
+        'reviews': reviews,
+    }
+    return render(request, 'restaurant_detail.html', context)
+
+def restaurant_list(request):
+    restaurants = Restaurant.objects.all() # do not change name of this variable
+
+    context = {}
+    context['restaurants'] = restaurants
+
+    return render(request, 'restaurant_list.html', context)
