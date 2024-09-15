0a5161afdd5091f17d500c4faf4c84650b01f408
aditikoratp <aditikoratp@gmail.com>
2024-09-15 16:20:06 -0400

    commit profile.html, templates, and url

--- foodie/urls.py
+++ foodie/urls.py
@@ -1,12 +1,7 @@
 from django.urls import path
-
 from . import views
 
-app_name = "polls"
 urlpatterns = [
-    # path("", views.IndexView.as_view(), name="index"),
-    # path("<int:pk>/", views.DetailView.as_view(), name="detail"),
-    # path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
-    # path("<int:question_id>/vote/", views.vote, name="vote"),
-    path("map", views.mapView, name = "mapView")
+    # ... other URL patterns ...
+    path('profile/', views.profile_view, name='profile'),
 ]
