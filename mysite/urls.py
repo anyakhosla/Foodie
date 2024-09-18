84474d8602e4725f72dc121ba120f02267be264b
anyakhosla <anyakhosla05@gmail.com>
2024-09-17 23:04:44 -0400

    add media url pattern

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.urls import path, include
 from django.shortcuts import redirect
+from django.conf import settings
+from django.conf .urls.static import static
 
 urlpatterns = [
     path('foodie/', include('foodie.urls')),
@@ -25,3 +27,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("foodie/", include("foodie.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
